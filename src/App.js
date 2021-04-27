a5a3c41cd2f1ed3f6449580275ff989e3704a269
Dzheremilz <trempejeremie@gmail.com>
2021-04-27 16:34:36 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [planets, setPlanets] = useState([])
+  const [page, setPage] = useState(1)
+  const [end, setEnd] = useState(false)
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/planets/?page=${page}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}`)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        if (data.next === null) {
+          setEnd(true)
+        }
+        console.log(data)
+        setPlanets(p => [...p, ...data.results])
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container py-5">
+      <h1 className="mb-5">Planètes dans l'univer Star Wars</h1>
+      <div className="row">
+        {planets.map((planet) => {
+          return (
+            <div className="col-md-6 col-lg-4 col-xl-3 mb-4" key={planet.name}>
+              <article className="bg-warning p-3">
+                <h2 className="h5">{planet.name}</h2>
+                <p className="mb-0"><b>population</b><br />{planet.population}</p>
+                <p className="mb-0"><b>climat</b><br />{planet.climate}</p>
+              </article>
+            </div>
+          )
+        })}
+      </div>
+      {end ? <p className="bg-dark text-white p-3">Nous avons listé toutes les planètes recensées.</p> : <button type="button" className="btn btn-dark" onClick={() => setPage(p => p + 1)} >Suivantes</button>}
+    </section>
   );
 }
 
